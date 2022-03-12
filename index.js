75be7a42c099e9e2ff27844eaad0e106243287c8
Nataliia Kovaliv <nataliiakovaliv95@gmail.com>
2022-03-12 19:22:07 +0200

    add database

--- index.js
+++ index.js
@@ -4,13 +4,38 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require('./config/db');
+const boardRoutes = require('./routes/board')
 
 
 const app = express();
 
 const PORT = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Main page');
+})
+
+app.use('/account', boardRoutes);
+
+mongoose.connect(config.db, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true 
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connection to database was successful')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log('Connection to db was not successful' + err)
+})
 
 app.listen(PORT, () => {
-  console.log('Server has been started..')
+  console.log('Server has been started')
 })
